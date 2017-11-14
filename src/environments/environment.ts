db30bf22da26e7ec77f2288a56a11d6e64bd5cfb
Hyo Lee <hyol@exsilio.com>
2017-11-14 11:17:43 -0800

    ...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   production: false,
   adalConfig: {
     tenant: 'hyoyoegmail.onmicrosoft.com',
-    clientId: 'f517cdbc-e3b9-4d75-a019-269421890c4f',
+    clientId: '3aff21c7-97b7-4699-b2e4-0859919c7650',
     postLogoutRedirectUri: 'https://login.microsoftonline.com/74938eab-1c7b-4d9c-8497-f9c3b262aae0/oauth2/logout',
     endpoints: {
       'https://angulartestapimssr.azurewebsites.net': 'https://angulartestapimssr.azurewebsites.net'
