da72223b4522c5d786879dbe4fab6f11e1af131a
Hyo Lee <hyol@exsilio.com>
2017-11-14 16:20:44 -0800

    Client Side Web Token working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,11 +19,11 @@ export class AppComponent implements OnInit {
   constructor(private oauthService: OAuthService) { }
 
   private async ConfigureAuth(): Promise<void> {
-    this.oauthService.loginUrl = 'https://login.microsoftonline.com/74938eab-1c7b-4d9c-8497-f9c3b262aae0/oauth2/token';
+    this.oauthService.loginUrl = 'https://login.microsoftonline.com/74938eab-1c7b-4d9c-8497-f9c3b262aae0/oauth2/authorize';
     this.oauthService.clientId = '10e7a440-1b2d-4396-9cf3-90a73d841648';
     this.oauthService.resource = 'https://hyoyoegmail.onmicrosoft.com/6eb52dd1-1712-49d1-9103-ed25485af3e3'; //backend AAD URI
     this.oauthService.logoutUrl = 'https://login.microsoftonline.com/74938eab-1c7b-4d9c-8497-f9c3b262aae0/oauth2/logout';
-    this.oauthService.redirectUri = 'http://angular4restapiclient.azurewebsites.net';  //window.location.origin + '/';
+    this.oauthService.redirectUri =  window.location.origin + '/';
     this.oauthService.scope = 'openid';
     this.oauthService.oidc = true;
     this.oauthService.setStorage(sessionStorage);
