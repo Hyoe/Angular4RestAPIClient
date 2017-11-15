69e99dae984c69e4ba89ac7584688931729b19c4
Hyo Lee <hyol@exsilio.com>
2017-11-15 11:48:46 -0800

    test api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { PhotoService } from './photo.service';
 import { PhotoComponent } from './photo.component';
 import { Photo } from './photo';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { Headers, Http, RequestOptions } from '@angular/http';
 
 @Component({  
   selector: 'app-root',  
@@ -16,22 +17,36 @@ import { OAuthService } from 'angular-oauth2-oidc';
 export class AppComponent implements OnInit {  
   title: string = "Angular Test";
 
-  constructor(private oauthService: OAuthService) { }
+  constructor(private oauthService: OAuthService, private http: Http) { }
 
   private async ConfigureAuth(): Promise<void> {
     this.oauthService.loginUrl = 'https://login.microsoftonline.com/74938eab-1c7b-4d9c-8497-f9c3b262aae0/oauth2/authorize'; //endpoint
+
+    this.oauthService.tokenEndpoint = 'https://login.microsoftonline.com/74938eab-1c7b-4d9c-8497-f9c3b262aae0/oauth2/token'; //endpoint
+
+
     this.oauthService.clientId = '10e7a440-1b2d-4396-9cf3-90a73d841648'; //application ID
     this.oauthService.resource = 'https://hyoyoegmail.onmicrosoft.com/6eb52dd1-1712-49d1-9103-ed25485af3e3'; //backend AAD URI
     this.oauthService.logoutUrl = 'https://login.microsoftonline.com/74938eab-1c7b-4d9c-8497-f9c3b262aae0/oauth2/logout'; //endpoint
     this.oauthService.redirectUri =  window.location.origin + '/';
-    this.oauthService.scope = 'openid';
+    //this.oauthService.scope = 'openid';
     this.oauthService.oidc = true;
     this.oauthService.setStorage(sessionStorage);
+    //this.oauthService.oidc = false;
+    this.oauthService.scope = 'openid profile email offline_access roles identity';
+    //this.oauthService.setupAutomaticSilentRefresh();
   }
 
   async ngOnInit() {
     await this.ConfigureAuth();
-    this.oauthService.tryLogin({});
+    this.oauthService.tryLogin({
+      validationHandler: context => {
+        var search = new URLSearchParams();
+        search.set('token', context.idToken); 
+        search.set('client_id', this.oauthService.clientId);
+        return this.http.get('https://login.microsoftonline.com/74938eab-1c7b-4d9c-8497-f9c3b262aae0/oauth2/authorize', { search }).toPromise();
+    }
+    });
 
     if (!this.oauthService.getAccessToken()) {
       await this.oauthService.initImplicitFlow();
@@ -43,6 +58,10 @@ export class AppComponent implements OnInit {
     console.log("has valid id token - " + this.oauthService.hasValidIdToken());
     console.log("has valid access token - " + this.oauthService.hasValidAccessToken());
     console.log("authorization header - " + this.oauthService.authorizationHeader());
+
+    // this.oauthService.refreshToken().then(() => {
+    //   console.debug('ok');
+    // })
   }
   
 }
